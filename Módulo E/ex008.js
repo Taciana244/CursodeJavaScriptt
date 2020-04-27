7337b3a19a1d0101de41e030eac925e94653510c
Taciana Santana <tacianasantana14@gmail.com>
2020-04-27 11:05:51 -0300

    Exercícios Java Script (parte 6)

--- Módulo E/ex008.js
+++ Módulo E/ex008.js
@@ -1,34 +1,22 @@
-function contar (){
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = documento.getElementById ('res')
-    if(in.value.length==0|| fim.value.length==0|| passo.vallue.length){
-        res.innerHTML='Impossível contar'
-       // window.alert('[erro] faltam dados !')
-        res.innerHTML='Impossível contar'
-    } else{
-        res.innerhtml='Contando:<br>'
-        let i= Number(ini.value)
-        let f=Number(fim.value)
-        let p = Number(passo.value)
-        if(p<= 0){
-            window.alert('Passo inválido!Considerando passo 1')
-            p=1
-        }
-        if(i<f){
-            //Contagem crescente
-            for(let c =i; c<=f; c += p) {
-                res.innerHTML += `${c} \u{1f449}`
-             }
-        }else{
-            //Contagem regressiva
-        for(let c =i; c<=f; c += p) {
-        res.innerhtml += `${c} \u{1f449}`
-        }
+function tabuada (){
 
-      }
-      res.innerHTML += ` \u{1f3c1}`
+    let num  = document.getElementById('txtn')
+    let tab = document.getElementById('txttab')
+    if(num.vale.length==0){
+
+     window.alert('por favor, digite um número !')
+
+    } else{
+        let n= Number(num.va)
+        let c = 1
+        tab.innerHTML=''
+        while(c<=10){
+            let item = document.createElement('option')
+            item.text=`${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+  
     
     }
 }
