de3b0a498d9f45eff646205647ac12bf160cac53
Taciana Santana <tacianasantana14@gmail.com>
2020-05-02 14:06:58 -0300

    Funções #16

--- Módulo F/vetornatela.js
+++ Módulo F/vetornatela.js
@@ -1,7 +1,8 @@
 let valores=[8,1,7,4,2,,9]
 
 console.log(valores)
- for (let pos in valores){
+ for (let pos in valores
+    ){
      console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
  }
 /*
